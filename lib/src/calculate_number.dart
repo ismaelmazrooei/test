ed0f423649d35d46ec1b7183d9cfe300faa66860
ismaelmazrooei <ismaelmazrooei@gmail.com>
2022-11-08 10:54:44 +0330

    به  روز رسانی پلاگین

--- lib/src/calculate_number.dart
+++ lib/src/calculate_number.dart
@@ -9,7 +9,19 @@ class Number {
   }
 
   /// این قسمت نمایش می دهد که این عدد اول است یا خیر
+  @Deprecated(
+      "متد زیر از چرخه کاری خارج شده و شما می توانید از متد زیر  برای این منظور استفاده کنید")
   String evenOradd(int number) {
+    if (number % 2 == 0) {
+      return "Even";
+    } else {
+      return "odd";
+    }
+  }
+
+  //this id method for  number ditect for odd
+  //این متد برای تشخیص اول بودن و بهینه سازی شده در اینقسمت قرار گرفته است
+  String newMethod(int number) {
     if (number % 2 == 0) {
       return "Even";
     } else {
