9e96b59b1d1dca62cd02fc106c7fa98707464fa4
ismaelmazrooei <ismaelmazrooei@gmail.com>
2022-11-08 10:40:09 +0330

    update

--- lib/src/calculate_number.dart
+++ lib/src/calculate_number.dart
@@ -1,4 +1,5 @@
 class Number {
+  ///این نمایش دهنده این است که آیااین عدد منفی است یا خیر
   bool isNegative(int number) {
     if (number < 0) {
       return true;
@@ -7,6 +8,7 @@ class Number {
     }
   }
 
+  /// این قسمت نمایش می دهد که این عدد اول است یا خیر
   String evenOradd(int number) {
     if (number % 2 == 0) {
       return "Even";
